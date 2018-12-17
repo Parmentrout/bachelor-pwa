0b0b98a962c37b4b61689fa69dd57c0fc17cede2
Patrick Armentrout <patrick.armentrout@gmail.com>
2018-12-17 16:38:59 -0700

    Get fantasy section complete

--- src/app/pages/fantasy/fantasy.component.ts
+++ src/app/pages/fantasy/fantasy.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { BachelorService } from 'src/app/bachelor-service.service';
+import { Contestant } from 'src/app/models';
+import { map, takeUntil } from 'rxjs/operators';
+import { Observable, Subject } from 'rxjs';
 
 @Component({
   selector: 'app-fantasy',
@@ -7,9 +11,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FantasyComponent implements OnInit {
 
-  constructor() { }
+  amberContestants: Contestant[] = new Array<Contestant>();
+
+  patrickContestants: Contestant[] = new Array<Contestant>();
+
+  isLoaded: boolean = false;
+
+  private _cancellationToken: Subject<void> = new Subject();
+
+  constructor(private _bachelorService: BachelorService) { }
 
   ngOnInit() {
+
+    this._bachelorService.getContestants().pipe(takeUntil(this._cancellationToken)).subscribe(results => {
+      this.amberContestants = this.filterContestants('Amber', results);
+      this.patrickContestants = this.filterContestants('Patrick', results);
+      this.isLoaded = true;
+    })
+  }
+
+  filterContestants(name: string, contestants: Contestant[]): Contestant[] {
+    let result = new Array<Contestant>();
+
+    for (let contestant of contestants) {
+      if (contestant.selectedBy === name) {
+        result.push(contestant);
+      }
+    }
+    console.log(result);
+    return result;
   }
 
 }
