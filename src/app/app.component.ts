5b0c1809de633b4da66d0548d41edd68593ccd99
Patrick Armentrout <patrick.armentrout@gmail.com>
2019-01-06 14:37:05 -0700

    Testing push notifications

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,8 @@
-import { Component, Inject } from '@angular/core';
+import { Component, Inject, AfterViewInit } from '@angular/core';
 import { Observable } from 'rxjs';
 import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 import { MatSnackBar, MAT_SNACK_BAR_DATA } from '@angular/material';
-import { SwUpdate } from '@angular/service-worker';
+import { SwUpdate, SwPush } from '@angular/service-worker';
 import { SnackBarListenerService } from './snackbar.service';
 
 @Component({
@@ -10,19 +10,17 @@ import { SnackBarListenerService } from './snackbar.service';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
 
-  constructor(private snackBar: MatSnackBar, private swUpdate: SwUpdate) {
+  private readonly vapidKey = "BN_IR7AHynzjzbh3nR3-yQFgnMjHRfndMDrXDFXLZAAWPCT1tMPdVcrL4RRSodK37CMT6gbaYjU8LQ0ExQL6oTM";
 
-    // Check for available updates
-    swUpdate.available.subscribe(event => 
-      this.openSnackBar(`Updates are available version: ${event.available}`, 'Update'));
+  constructor(private snackBar: MatSnackBar, private swUpdate: SwUpdate, private swPush: SwPush) {
 
-    // Check if updates have been applied
-    swUpdate.activated.subscribe(event => {
-      this.openSnackBar(`Bachelor Tracker Updated: ${event.current}`);
-    })
+    this.subscribeToUpdates();
+  }
 
+  ngAfterViewInit() {
+    this.subscribeToPushNotifications();
   }
 
   openSnackBar(message: string, action: string = null) {
@@ -34,5 +32,27 @@ export class AppComponent {
       this.snackBar.open(message);
     }
    
+  }
+
+  private subscribeToUpdates() {
+      // Check for available updates
+      this.swUpdate.available.subscribe(event => 
+      this.openSnackBar(`Updates are available version: ${event.available}`, 'Update'));
+
+      // Check if updates have been applied
+      this.swUpdate.activated.subscribe(event => {
+      this.openSnackBar(`Bachelor Tracker Updated: ${event.current}`);
+    });
+  }
+
+  private subscribeToPushNotifications() {
+   
+    this.swPush.requestSubscription({
+      serverPublicKey: this.vapidKey
+    })
+    .then(result => console.log('User successfully subscribed to notifications')) // Returns a user specific key that in turn you'll send with other notifications
+    .catch(error => console.log('User said no'));
+
+    this.swPush.messages.subscribe(message => console.log(`Message received: ${message}`));
   }
 }
