f8fa402fd338ab56474b452ca4cd699a4473e63c
Patrick Armentrout <patrick.armentrout@gmail.com>
2020-01-15 20:22:40 -0700

    Enhancement request

--- src/app/pages/scoreboard/scoreboard.component.ts
+++ src/app/pages/scoreboard/scoreboard.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { BachelorService } from 'src/app/bachelor-service.service';
 import { Observable } from 'rxjs';
 import { Season } from 'src/app/models';
+import { take } from 'rxjs/operators';
 
 @Component({
   selector: 'app-scoreboard',
@@ -11,10 +12,25 @@ import { Season } from 'src/app/models';
 export class ScoreboardComponent {
 
   seasons$: Observable<Season[]> = this._bachelorService.getScoreboard();
+  amberBachScore = 0;
+  patrickBachScore = 0;
 
+  amberWinnerScore = 0;
+  patrickWinnerScore = 0;
   constructor(private _bachelorService: BachelorService) { }
 
+
   ngOnInit() {
-  }
+
+    this.seasons$.pipe(take(1)).subscribe(results => {
+
+      for (let result of results) {
+        if (result.pickedBach === "Amber") { this.amberBachScore++;}
+        if (result.pickedBach === "Patrick") { this.patrickBachScore++;}
+        if (result.pickedWinner === "Amber") { this.amberWinnerScore++;}
+        if (result.pickedWinner === "Patrick") { this.patrickWinnerScore++;}
+      }
+  });
+}
 
 }
